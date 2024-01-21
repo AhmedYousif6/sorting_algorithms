1a798f1b449b672e30adf36dc33fbd3e0cd1b579
hibaasim <hiba.asim10@gamil.com>
2024-01-21 14:23:46 +0200

    fixing task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,25 +11,27 @@
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, k;
-	int min;
+	int temp;
+	/*int min = 0;*/
 
-	if (!array || !size)
+	if (!array || size < 2)
 		return;
 	for (i = 0; i < (size - 1); i++)
 	{
-		min = array[i];
+		k = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (min > array[j])
+			if (array[k] > array[j])
 			{
-				min = array[j];
 				k = j;
 			}
+			
 		}
 		if (k != i)
 		{
+			temp = array[k];
 			array[k] = array[i];
-			array[i] = min;
+			array[i] = temp;
 			print_array(array, size);
 		}
 	}
