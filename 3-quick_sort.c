7891f5d8aa1ac922d6726e29b8bcefcab3f00dbf
AhmedYousif6 <ahmedmax0909090909@gmail.com>
2024-01-23 15:33:04 -0800

    fix print array

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,16 +1,15 @@
 #include "sort.h"
 
 /**
- * quick_sort - 
+ * quick_sort - sorts an array of integers.
  * @array: the integers' array
  * @size: the array size
- * 
  * Return: Nothing
-*/
+ */
 void quick_sort(int *array, size_t size)
 {
 	size_t low, high;
-	
+
 	if (!array || size < 2)
 		return;
 	low = 0;
@@ -23,32 +22,19 @@ void quick_sort(int *array, size_t size)
  * @array: the array
  * @low: the first value
  * @high: the last value
- * 
  * Return: nothing
-*/
-void quicksort(int *array, size_t low, size_t high)
+ */
+void quicksort(int *array, int low, int high)
 {
-	size_t pivot, i, j;
+	size_t pivot;
 
-	j = low;
 	if (low >= high)
 		return;
 	if (low < high)
 	{
 		pivot = lomuto(array, low, high);
-		/*print_array(array, (high + 1));*/
 		quicksort(array, (pivot + 1), high);
-		/*if (low != high)
-			print_array(array, (10));*/
 		quicksort(array, low, (pivot - 1));
-		for (i = 0; i < high + 1; i++)
-		{
-			if (array[i] == array[j])
-				continue;
-			j++;
-		}
-		print_array(array, (10));
-		/*print_array(array, (10));*/
 	}
 }
 
@@ -57,30 +43,34 @@ void quicksort(int *array, size_t low, size_t high)
  * @array: the array to be parted
  * @low: the first value
  * @high: the highest value
- * 
  * Return: the index of the new pivot
-*/
-size_t lomuto(int *array, size_t low, size_t high)
-{ 
-	int pivot = array[high], tmp, tmp2;
-	size_t j, i;
+ */
+int lomuto(int *array, int low, int high)
+{
+	int pivot = array[high], tmp;
+	int j, i;
 
-	i = low - 1;
+	i = low;
 	for (j = low; j < high; j++)
 	{
-		if(pivot >= array[j])
+		if (pivot >= array[j])
 		{
-			i += 1;
-			tmp = array[i];
-			array[i] = array[j];
-			array[j] = tmp;
+			if (i != j)
+			{
+				tmp = array[i];
+				array[i] = array[j];
+				array[j] = tmp;
+				print_array(array, (10));
+			}
+			i++;
 		}
 	}
-	if ( i != j)
+	if (i != high)
+	{
+		tmp2 = array[i];
+		array[i] = array[high];
+		array[high] = tmp2;
 		print_array(array, (10));
-	i += 1;
-	tmp2 = array[i];
-	array[i] = array[high];
-	array[high] = tmp2;
+	}
 	return (i);
 }
--- sort.h
+++ sort.h
@@ -30,7 +30,7 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-void quicksort(int *array, size_t low, size_t high);
-size_t lomuto(int *array, size_t low, size_t high);
+void quicksort(int *array, int low, int high);
+int lomuto(int *array, int low, int high);
 
 #endif
